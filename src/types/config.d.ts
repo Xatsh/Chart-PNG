35ca3ac5b6c91474443e560c0955f08f26c1aa95
Xat <i@xat.sh>
2024-12-31 14:00:58 +0800

    feat: polish type defination

--- src/types/config.d.ts
+++ src/types/config.d.ts
@@ -1,10 +1,10 @@
-import type { ChartConfiguration, ChartTypeRegistry } from "chart.js"
+import type { ChartConfiguration, ChartType, DefaultDataPoint } from "chart.js"
 import type { AnnotationOptions } from "chartjs-plugin-annotation"
 import type { AutocolorsOptions } from "chartjs-plugin-autocolors"
 import type { Options as DatalabelsOptions } from "chartjs-plugin-datalabels/types/options"
 import type { Options as GradientOptions } from "chartjs-plugin-gradient/types/options"
 
-export type Config<TType extends keyof ChartTypeRegistry = keyof ChartTypeRegistry> = ChartConfiguration<TType> & {
+export type Config<TType extends ChartType = ChartType, TData = DefaultDataPoint<TType>, TLabel = unknown> = ChartConfiguration<TType, TData, TLabel> & {
 	data?: ChartConfiguration<TType>["data"] & {
 		datasets?: ChartConfiguration<TType>["data"]["datasets"] & {
 			dataLabels?: DatalabelsOptions
