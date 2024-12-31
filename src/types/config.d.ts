8f15a05cdc9d89ae31d02de3ee850c23af34e11e
Xat <i@xat.sh>
2024-12-31 14:08:55 +0800

    fix: missing type in Conifg

--- src/types/config.d.ts
+++ src/types/config.d.ts
@@ -1,17 +1,17 @@
-import type { ChartConfiguration, ChartType, DefaultDataPoint } from "chart.js"
+import type { ChartConfiguration, ChartData, ChartType, DefaultDataPoint } from "chart.js"
 import type { AnnotationOptions } from "chartjs-plugin-annotation"
 import type { AutocolorsOptions } from "chartjs-plugin-autocolors"
 import type { Options as DatalabelsOptions } from "chartjs-plugin-datalabels/types/options"
 import type { Options as GradientOptions } from "chartjs-plugin-gradient/types/options"
 
 export type Config<TType extends ChartType = ChartType, TData = DefaultDataPoint<TType>, TLabel = unknown> = ChartConfiguration<TType, TData, TLabel> & {
-	data?: ChartConfiguration<TType>["data"] & {
-		datasets?: ChartConfiguration<TType>["data"]["datasets"] & {
+	data: ChartConfiguration<TType, TData, TLabel>["data"] & {
+		datasets: ChartConfiguration<TType, TData, TLabel>["data"]["datasets"] & {
 			dataLabels?: DatalabelsOptions
 			gradient?: GradientOptions
 		}
 	}
-	options?: ChartConfiguration<TType>["options"] & {
+	options?: ChartConfiguration<TType, TData, TLabel>["options"] & {
 		plugins?: {
 			annotation?: AnnotationOptions
 			autocolors?: AutocolorsOptions
