95bb8b0ac9d46dd7a3507de14ca547b9ed64bf20
Xat <i@xat.sh>
2024-12-31 14:30:09 +0800

    fix: i mess up everything

--- src/types/config.d.ts
+++ src/types/config.d.ts
@@ -1,14 +1,12 @@
 import type { ChartConfiguration, ChartData, ChartType, DefaultDataPoint } from "chart.js"
-import type { AnnotationOptions } from "chartjs-plugin-annotation"
+import type { AnnotationPluginOptions as AnnotationOptions } from "chartjs-plugin-annotation"
 import type { AutocolorsOptions } from "chartjs-plugin-autocolors"
 import type { Options as DatalabelsOptions } from "chartjs-plugin-datalabels/types/options"
-import type { Options as GradientOptions } from "chartjs-plugin-gradient/types/options"
 
 export type Config<TType extends ChartType = ChartType, TData = DefaultDataPoint<TType>, TLabel = unknown> = ChartConfiguration<TType, TData, TLabel> & {
 	data: ChartConfiguration<TType, TData, TLabel>["data"] & {
 		datasets: Array<ChartDataset<TType, TData> & {
 			dataLabels?: DatalabelsOptions
-			gradient?: GradientOptions
 		}>
 	}
 	options?: ChartConfiguration<TType, TData, TLabel>["options"] & {
